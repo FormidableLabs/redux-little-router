2cee78e4ec6fbcde533bd1c68c66f976b443b046
Tyler Thompson <tyler@tylerpaulthompson.com>
2016-08-18 10:39:13 -0700

    7.3.0

--- dist/redux-little-router.js
+++ dist/redux-little-router.js
@@ -2703,9 +2703,9 @@ return /******/ (function(modules) { // webpackBootstrap
 	});
 	exports.PersistentQueryLink = exports.Link = undefined;
 	
-	var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
-	
 	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+	
+	var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
 	
 	var _react = __webpack_require__(30);
 	
@@ -2715,13 +2715,13 @@ return /******/ (function(modules) { // webpackBootstrap
 	
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 	
-	function _objectWithoutProperties(obj, keys) { var target = {}; for (var i in obj) { if (keys.indexOf(i) >= 0) continue; if (!Object.prototype.hasOwnProperty.call(obj, i)) continue; target[i] = obj[i]; } return target; }
-	
 	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 	
 	function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
 	
 	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+	
+	function _objectWithoutProperties(obj, keys) { var target = {}; for (var i in obj) { if (keys.indexOf(i) >= 0) continue; if (!Object.prototype.hasOwnProperty.call(obj, i)) continue; target[i] = obj[i]; } return target; }
 	
 	var LEFT_MOUSE_BUTTON = 0;
 	
@@ -2792,6 +2792,9 @@ return /******/ (function(modules) { // webpackBootstrap
 	  var persistQuery = props.persistQuery;
 	  var replaceState = props.replaceState;
 	  var children = props.children;
+	
+	  var rest = _objectWithoutProperties(props, ['href', 'target', 'persistQuery', 'replaceState', 'children']);
+	
 	  var router = context.router;
 	
 	
@@ -2805,9 +2808,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	
 	  return _react2.default.createElement(
 	    'a',
-	    {
-	      className: props.className,
-	      style: props.style,
+	    _extends({
 	      href: normalizeHref(location),
 	      onClick: function onClick(e) {
 	        return _onClick({
@@ -2818,7 +2819,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	          router: router
 	        });
 	      }
-	    },
+	    }, rest),
 	    children
 	  );
 	};
