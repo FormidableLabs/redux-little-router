ed5887755b66869d4a9b9cabdbf50d3370c66fcf
Brandon Dail <brandon.dail@formidable.com>
2016-10-17 17:41:41 -0500

    Add generate-id utility
    A small utility function for generating ids that are almost certainly
    unique, even with thousands of fragments.

---
+++ src/util/generate-id.js
@@ -0,0 +1,11 @@
+/**
+ * Returns a psuedo-unique identifier used by fragments
+ * to track match status within MatchCache.
+ * @returns {String} id
+ * @flow
+ */
+export default (): string => {
+  const radix = 16;
+  const length = 8;
+  return (Math.random() * Date.now()).toString(radix).slice(0, length);
+};
