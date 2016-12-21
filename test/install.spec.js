feceab27d8a6c4ce83f81c18a1ac67264ccccb92
Tyler Thompson <tyler.thompson@formidable.com>
2016-12-20 23:49:42 -0800

    Test that install adds route match results to the initial location

--- test/install.spec.js
+++ test/install.spec.js
@@ -2,8 +2,28 @@ import { expect } from 'chai';
 
 import install from '../src/install';
 
-// eslint-disable-next-line max-statements
 describe('Router installer', () => {
+  it('appends the match result to the location passed to the reducer factory', () => {
+    const { reducer } = install({
+      routes: {
+        '/:thing': { congratulations: 'you played yourself' }
+      },
+      history: {},
+      location: { pathname: '/stuff' }
+    });
+
+    expect(reducer(undefined, {})).to.deep.equal({
+      pathname: '/stuff',
+      params: {
+        thing: 'stuff'
+      },
+      route: '/:thing',
+      result: {
+        congratulations: 'you played yourself'
+      }
+    });
+  });
+
   it('throws if no routes are provided', () => {
     expect(() => install({
       routes: null,
