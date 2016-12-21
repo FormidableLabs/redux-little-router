5db70bed55b1c77f6b5bc4dad827721f2b4357bd
Tyler Thompson <tyler.thompson@formidable.com>
2016-12-20 23:26:17 -0800

    Update type declarations for history v4

--- interfaces/history.js
+++ interfaces/history.js
@@ -1,55 +1,88 @@
 declare module 'history' {
-  declare type HistoryOptions = Object;
-
   declare type Action = 'PUSH' | 'REPLACE' | 'POP';
-  declare type Hash = string;
-  declare type Href = string;
-  declare type Path = string;
-  declare type Pathname = string;
-  declare type Query = Object;
-  declare type Search = string;
-
-  declare type LocationKey = string;
-  declare type LocationState = ?Object;
 
   declare type Location = {
-    pathname: Pathname;
-    search: Search;
-    query: Query;
-    state: LocationState;
+    pathname: string,
+    search?: string,
+    hash?: string,
+    state?: Object,
+    key?: string
+  };
+
+  declare type ListenCallback = (location: Location, action?: Action) => void;
+  declare type BlockCallback = (location: Location, action?: Action) => string;
+  declare type Unsubscribe = () => void;
+
+  declare class History {
+    length: number;
+    location: Location;
+
     action: Action;
-    key: LocationKey;
-  };
 
-  declare type LocationDescriptorObject = {
-    pathname: Pathname;
-    search?: Search;
-    query?: Query;
-    state?: LocationState;
-  };
+    listen(callback: ListenCallback): Unsubscribe;
 
-  declare type LocationDescriptor = LocationDescriptorObject | Path;
+    push(path: string, state?: Object): void;
+    push(location: Location): void;
+    replace(path: string, state?: Object): void;
+    replace(location: Location): void;
 
-  declare type LocationListener = (location: Location) => void;
+    go(n: number): void;
+    goBack(): void;
+    goForward(): void;
 
-  declare type TransitionHook = (location: Location, callback: ?Function) => any;
+    block(message: string): void;
+    block(callback: BlockCallback): Unsubscribe;
+  }
 
-  declare type History = {
-    listenBefore: (hook: TransitionHook) => Function,
-    listen: (listener: LocationListener) => Function,
-    transitionTo: (location: Location) => void,
-    push: (location: LocationDescriptor) => void,
-    replace: (location: LocationDescriptor) => void,
-    go: (n: number) => void,
-    goBack: () => void,
-    goForward: () => void,
-    createKey: () => LocationKey,
-    createPath: (location: LocationDescriptor) => Path,
-    createHref: (location: LocationDescriptor) => Href,
-    createLocation: (location: LocationDescriptor, action: ?Action, key: ?LocationKey) => Location
-  };
+  declare class MemoryHistory extends History {
+    index: number;
+    entries: Array<string>;
 
-  declare type BeforeUnloadHook = () => ?string;
-  declare type CreateHistory = (options: ?HistoryOptions) => History;
-  declare type CreateHistoryEnhancer = (createHistory: CreateHistory) => CreateHistory;
+    canGo(n: number): void;
+  }
+
+  declare type GetUserConfirmation = (
+    message: string,
+    callback: (continueTransition: bool) => void
+  ) => void;
+
+  declare type BrowserHistoryOptions = {|
+    basename?: string,
+    forceRefresh?: bool,
+    keyLength?: number,
+    getUserConfirmation: GetUserConfirmation;
+  |};
+
+  declare function createBrowserHistory(options?: BrowserHistoryOptions): History;
+
+  declare type MemoryHistoryOptions = {|
+    initialEntries?: Array<string>,
+    initialIndex?: number,
+    keyLength?: number,
+    getUserConfirmation?: GetUserConfirmation
+  |};
+
+  declare function createMemoryHistory(options?: MemoryHistoryOptions): MemoryHistory;
+
+  declare type HashType = 'slash' | 'noslash' | 'hashbang';
+
+  declare type HashHistoryOptions = {|
+    basename?: string,
+    hashType?: HashType,
+    getUserConfirmation?: GetUserConfirmation
+  |};
+
+  declare function createHashHistory(options?: HashHistoryOptions): History;
+
+  declare function createLocation(
+    path: string | Location,
+    state?: any,
+    key?: string,
+    currentLocation?: Location
+  ): Location;
+
+  declare function locationsAreEqual(a: Location, b: Location): bool;
+
+  declare function parsePath(path: string): Location;
+  declare function createPath(location: Location): string;
 }
