291f0d3ac06c07356a6fb6e348e153fc6fbf42ca
Stuart Knightley <stuart@stuartk.com>
2017-06-06 00:14:59 -0700

    Remove location init code from hash-router
    The history package does all of this, but also handles query strings.
    The only downside is we have to manipulate location.hash in the tests,
    but that works fine.

--- src/environment/hash-router.js
+++ src/environment/hash-router.js
@@ -8,48 +8,19 @@ type HashRouterArgs = {
   routes: Object,
   basename: string,
   hashType: string,
-  getLocation: () => Location,
   passRouterStateToReducer?: bool
 };
-
-/* istanbul ignore next: unstubbable! */
-const realLocation = () => window.location;
-
-const HASH = /^#/;
-const HASH_BANG = /^#!/;
 
 export default ({
   routes,
   basename,
-  hashType = 'slash',
-  getLocation = realLocation
+  hashType = 'slash'
 }: HashRouterArgs) => {
   const history = createHashHistory({ basename, hashType });
 
-  const { hash = '', search } = getLocation();
-
-  let fullPathname;
-  switch (hash) {
-  case 'noslash':
-    fullPathname = `/${ hash.replace(HASH, '') }`;
-    break;
-  case 'hashbang':
-    fullPathname = hash.replace(HASH_BANG, '');
-    break;
-  // Default is slash
-  default:
-    fullPathname = hash.replace(HASH, '');
-    break;
-  }
-
-  // Strip the basename from the initial pathname
-  const pathname = basename
-    ? fullPathname.replace(basename, '')
-    : fullPathname;
-
   const descriptor = basename
-    ? { pathname, basename, search }
-    : { pathname, search };
+    ? { basename, ...history.location }
+    : history.location;
 
   const location = normalizeHref(descriptor);
 
