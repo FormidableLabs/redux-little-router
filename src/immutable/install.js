4645ce52435006b142ff0f856e897de2dcc3d557
Stefan Huynh <huynh.stefan@gmail.com>
2017-12-01 15:01:46 -0800

    Add immutable install

--- src/immutable/install.js
+++ src/immutable/install.js
@@ -3,6 +3,6 @@ import reducer from './reducer';
 import middleware from './middleware';
 import enhancer from './enhancer';
 
-import createInstaller from './util/create-installer';
+import createInstaller from '../util/create-installer';
 
 export default createInstaller({ reducer, middleware, enhancer });
---
+++ src/util/create-installer.js
@@ -0,0 +1,44 @@
+// @flow
+import type { History } from 'history';
+import type { Location } from '../types';
+
+import { default as matcherFactory } from './create-matcher';
+import validateRoutes from './validate-routes';
+import flattenRoutes from './flatten-routes';
+
+type InstallArgs = {|
+  routes: Object,
+  history: History,
+  location: Location,
+  createMatcher?: Function
+|};
+
+export default ({ reducer, middleware, enhancer }) =>
+  ({
+    routes: nestedRoutes,
+    history,
+    location,
+    createMatcher = matcherFactory
+  }: InstallArgs) => {
+    validateRoutes(nestedRoutes);
+    const routes = flattenRoutes(nestedRoutes);
+    const matchRoute = createMatcher(routes);
+
+    return {
+      reducer: reducer({
+        routes,
+        initialLocation: {
+          ...location,
+          ...matchRoute(location.pathname)
+        }
+      }),
+      middleware: middleware({ history }),
+      enhancer: enhancer({
+        history,
+        matchRoute,
+        createMatcher
+      })
+    };
+  };
+
+
