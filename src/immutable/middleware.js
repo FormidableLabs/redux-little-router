4805e27ebededfeab3efdf70ec406592e65c9bb4
Stefan Huynh <huynh.stefan@gmail.com>
2017-12-05 11:49:23 -0800

    Add immutable middleware

---
+++ src/immutable/middleware.js
@@ -0,0 +1,12 @@
+// @flow
+import { handleNavigationAction } from '../middleware';
+
+export default ({ history }: MiddlewareArgs) =>
+  ({ getState }: Store<S, *>) =>
+    (next: Dispatch<*>) =>
+      (action: RouterAction) => {
+        const query = getState().getIn(['router', 'query']);
+        return isNavigationAction(action) ?
+          handleNavigationAction({ next, action, history, query }) :
+          next(action);
+      };
