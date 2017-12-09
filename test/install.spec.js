d7d01ab8d15f1688fa22f31d23fd9e50048d230d
Stefan Huynh <huynh.stefan@gmail.com>
2017-12-08 18:21:30 -0800

    Use loop strategy on install spec

--- test/install.spec.js
+++ test/install.spec.js
@@ -3,30 +3,46 @@ import { expect } from 'chai';
 import install from '../src/install';
 import immutableInstall from '../src/immutable/install';
 
-describe('Router installer', () => {
-  const setupTest = (installArg) => ({
-    router: install(installArg),
-    immutableRouter: immutableInstall(installArg)
-  });
+const installTest = {
+  install,
+  getState: (state) => state,
+  testLabel: 'router installer'
+};
+const immutableInstallTest = {
+  install: immutableInstall,
+  getState: (state) => state.toJS(),
+  testLabel: 'immutable router installer'
+};
 
-  const setupAsyncTest = (installArg) => ({
-    runInstall: () => install(installArg),
-    runImmutableInstall: () => immutableInstall(installArg)
-  });
-
-  it('appends the match result to the location passed to the reducer factory', () => {
-    const { router, immutableRouter } = setupTest({
-      routes: {
-        '/:thing': {
-          congratulations: 'you played yourself'
-        }
-      },
-      history: {},
-      location: { pathname: '/stuff' }
+[installTest, immutableInstallTest].forEach(({
+  install,
+  getState,
+  testLabel
+}) => {
+  describe('Router installer', () => {
+    const setupTest = (installArg) => ({
+      router: install(installArg),
+      immutableRouter: immutableInstall(installArg)
     });
-    [router, immutableRouter].forEach(({ reducer }) => {
-      const state = reducer(undefined, {});
-      expect(state.toJS ? state.toJS() : state).to.deep.equal({
+
+    const setupAsyncTest = (installArg) => ({
+      runInstall: () => install(installArg),
+      runImmutableInstall: () => immutableInstall(installArg)
+    });
+
+    it('appends the match result to the location passed to the reducer factory', () => {
+      const { reducer } = install({
+        routes: {
+          '/:thing': {
+            congratulations: 'you played yourself'
+          }
+        },
+        history: {},
+        location: { pathname: '/stuff' }
+      });
+      const state = getState(reducer(undefined, {}));
+
+      expect(state).to.deep.equal({
         pathname: '/stuff',
         params: {
           thing: 'stuff'
@@ -43,23 +59,25 @@ describe('Router installer', () => {
         queue: []
       });
     });
-  });
 
-  it('throws if no routes are provided', () => {
-    const { runInstall, runImmutableInstall } = setupAsyncTest({ routes: null, history: {}, location: {} });
-    [runInstall, runImmutableInstall].forEach(run => expect(run).to.throw(Error));
-  });
-
-  it('throws if malformed routes are provided', () => {
-    const { runInstall, runImmutableInstall } = setupAsyncTest({
-      routes: {
-        jlshdkfjgh: {},
-        '/real-route': {},
-        w: 'tf'
-      },
-      history: {},
-      location: {}
+    it('throws if no routes are provided', () => {
+      expect(() => install({
+        routes: {},
+        history: {},
+        location: {}
+      })).to.throw(Error);
     });
-    [runInstall, runImmutableInstall].forEach(run => expect(run).to.throw(Error));
+
+    it('throws if malformed routes are provided', () => {
+      expect(() => install({
+        routes: {
+          jlshdkfjgh: {},
+          '/real-route': {},
+          w: 'tf'
+        },
+        history: {},
+        location: {}
+      })).to.throw(Error);
+    });
   });
 });
