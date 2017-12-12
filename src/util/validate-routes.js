1275416078d8948e11219c134d3a5e4d7b9b101f
Stefan Huynh <huynh.stefan@gmail.com>
2017-12-12 13:51:28 -0800

    Add throwError util

--- src/util/validate-routes.js
+++ src/util/validate-routes.js
@@ -1,4 +1,6 @@
 // @flow
+import throwError from './throw-error';
+
 const README_MESSAGE = `
   See the README for more information:
   https://github.com/FormidableLabs/redux-little-router#wiring-up-the-boilerplate
@@ -6,26 +8,22 @@ const README_MESSAGE = `
 
 export default (routes: Object) => {
   if (!routes) {
-    throw Error(
-      `
+    throwError(`
       Missing route configuration. You must define your routes as
       an object where the keys are routes and the values are any
       route-specific data.
 
       ${README_MESSAGE}
-    `
-    );
+    `)();
   }
 
   // eslint-disable-next-line no-magic-numbers
   if (!Object.keys(routes).every(route => route.indexOf('/') === 0)) {
-    throw Error(
-      `
+    throwError(`
       The route configuration you provided is malformed. Make sure
       that all of your routes start with a slash.
 
       ${README_MESSAGE}
-    `
-    );
+    `)();
   }
 };
